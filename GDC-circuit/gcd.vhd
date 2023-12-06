fd0f166c5db6ee142b7e8ba32cc112e4a7dbb993
Damian Stone <63305840+damianstone@users.noreply.github.com>
2023-12-06 11:03:59 -0800

    Update gcd.vhd

--- GDC-circuit/gcd.vhd
+++ GDC-circuit/gcd.vhd
@@ -1,9 +1,10 @@
+
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 USE IEEE.std_logic_arith.ALL;
 
 entity GCD is
-  port(
+  port (
     clk, reset, start : in std_logic;
     Ain, Bin: in std_logic_vector(7 downto 0);
     GCDout: out std_logic_vector(7 downto 0);
@@ -16,23 +17,22 @@ architecture BEHAV of GCD is
     port (
       start, reset, clk : in STD_LOGIC;
       AeqB, AltB : in STD_LOGIC;
-      selA, selB, loadA, loadB, loadGCD, done : out STD_LOGIC;
+      selA, selB, loadA, loadB, loadGCD, done : out STD_LOGIC
     );
-  end component;
+  end component;      AeqB, AltB : out STD_LOGIC;      AeqB, AltB : out STD_LOGIC;      AeqB, AltB : out STD_LOGIC;
 
   component DATAPATH
     port (
       reset, clk, selA, selB, loadA, loadB, loadGCD : in STD_LOGIC;
-      AeqB, AltB : out STD_LOGIC;
       Ain : in STD_LOGIC_VECTOR(7 DOWNTO 0);
       Bin : in STD_LOGIC_VECTOR(7 DOWNTO 0);
       GCD : out STD_LOGIC_VECTOR(7 DOWNTO 0);
-    );
+      AeqB, AltB : out STD_LOGIC);
   end component;
 
   signal equal, lessthan, sa, sb, la, lb, loadgcd: STD_LOGIC;
 
-  begin
-    datapath : DATAPATH port map (reset, clk, sa, sb, la, lb, loadgcd, Ain, Bin, GDCOut);
-    controller : CONTROLLER port map(start, reset, clk, equal, lessthan, sa, sb, la, lb, loadgcd, done);
+begin
+  d : DATAPATH port map (reset, clk, sa, sb, la, lb, loadgcd, Ain, Bin, GCDOut, equal, lessthan);
+  c : CONTROLLER port map(start, reset, clk, equal, lessthan, sa, sb, la, lb, loadgcd, done);
 end BEHAV;
